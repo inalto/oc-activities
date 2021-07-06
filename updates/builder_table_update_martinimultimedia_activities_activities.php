d37a20bd75c351c2014e9dd27799da5206cde098
Alain Martini <webmaster@inalto.com>
2021-07-06 05:00:17 -0400

    jsonable fix

---
+++ updates/builder_table_update_martinimultimedia_activities_activities.php
@@ -0,0 +1,23 @@
+<?php namespace MartiniMultimedia\Activities\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateMartinimultimediaActivitiesActivities extends Migration
+{
+    public function up()
+    {
+        Schema::table('martinimultimedia_activities_activities', function($table)
+        {
+            $table->text('points')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('martinimultimedia_activities_activities', function($table)
+        {
+            $table->dropColumn('points');
+        });
+    }
+}
