905106320298c6fe324840d0a4373d0ecc375958
Alain Martini <webmaster@inalto.com>
2021-07-06 05:24:30 -0400

    timetable db fix

---
+++ updates/builder_table_update_martinimultimedia_activities_activities_4.php
@@ -0,0 +1,23 @@
+<?php namespace MartiniMultimedia\Activities\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateMartinimultimediaActivitiesActivities4 extends Migration
+{
+    public function up()
+    {
+        Schema::table('martinimultimedia_activities_activities', function($table)
+        {
+            $table->text('timetable')->nullable()->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('martinimultimedia_activities_activities', function($table)
+        {
+            $table->text('timetable')->nullable(false)->change();
+        });
+    }
+}
